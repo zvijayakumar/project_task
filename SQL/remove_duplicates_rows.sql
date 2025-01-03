694986d59ac6a6adc99e4e44ed4bc8db7b78a91b
zvijayakumar <zvijayakumar@gmail.com>
2025-01-03 17:10:25 +0100

    Added the ddl

--- SQL/remove_duplicates_rows.sql
+++ SQL/remove_duplicates_rows.sql
@@ -3,10 +3,26 @@
 --Your task is to write a SQL query that removes these duplicates and ensures only one record is retained for each route, irrespective of direction. 
 --The resulting table should list the routes in ascending order of city names.
 
+CREATE TABLE city_distances (
+    start_city VARCHAR(255) NOT NULL,
+    end_city VARCHAR(255) NOT NULL,
+    distance INT NOT NULL,
+    PRIMARY KEY (start_city, end_city)
+);
+
+-- Insert the example data:
+INSERT INTO city_distances (start_city, end_city, distance) VALUES
+('Mumbai', 'Kolkata', 1000),
+('Kolkata', 'Mumbai', 1000),
+('Chennai', 'Delhi', 1500),
+('Delhi', 'Chennai', 1500),
+('Kashmir', 'Kanyakumari', 1800),
+('Kanyakumari', 'Bangalore', 1800);
+
 --answer 2 
 select distinct case when cd.start_city < cd.end_city then cd.start_city else end_city end as start_city,
 case when cd.start_city < cd.end_city then cd.end_city else start_city end as end_city,cd.distance
-from city_distances cd 
+from city_distances cd ;
 
 
 
@@ -26,7 +42,7 @@ ON
     c1.start_city = c2.end_city
     AND c1.end_city = c2.start_city
     AND c1.start_city < c2.start_city
-    where c2.start_city is null 
+    where c2.start_city is null ;
     
     
     --answer 1
